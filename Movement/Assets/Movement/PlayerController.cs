1d66ad8f28118d8c40e7f3b90a75d573056906c4
Betapig1 <rynahall607@gmail.com>
2023-03-15 15:17:36 -0600

    Added disabling on hit

--- Movement/Assets/Movement/PlayerController.cs
+++ Movement/Assets/Movement/PlayerController.cs
@@ -391,6 +391,24 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector3(rb.velocity.x * 1.4f, 0f, rb.velocity.z);
 
             rb.AddForce(transform.up * (jumpForce / 1.6f), ForceMode.Impulse);
+            Vector3 local = FindObjectOfType<Waypoints>().gameObject.transform.localScale;
+            local.y = 1f;
+            FindObjectOfType<Waypoints>().gameObject.transform.localScale = local;
+
+
+            FindObjectOfType<Waypoints>().enabled = false;
+            Invoke("reEnable", 2f);
         }
+    }
+
+    void reEnable()
+    {
+        Vector3 local = FindObjectOfType<Waypoints>().gameObject.transform.localScale;
+        local.y = 1.343615f;
+        FindObjectOfType<Waypoints>().gameObject.transform.localScale = local;
+
+
+        FindObjectOfType<Waypoints>().enabled = true;
+
     }
 }
